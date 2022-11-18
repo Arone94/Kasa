07275cd6cf5d2df9259f5dde351280deb8f2931b
Arone Marchet <arone.marchet@gmail.com>
2022-11-18 16:11:15 +0100

    ajout des routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './style/index.scss';
 import {Routes, Route} from "react-router-dom";
+import './style/index.scss';
+import Home from './pages/Home';
+import About from './pages/About';
+import Logements from './pages/Logements';
+import Error from './pages/Error';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Navbar/>
-    </>
+        <Routes>
+          <Route path="/" element={ <Home /> }/>
+          <Route path='/a-propos' element={ <About /> }/>
+          <Route path='/logement/:id' element={ <Logements /> }/>
+          <Route path='*' element={ <Error /> }/>
+        </Routes>
+      <Footer/>
+    </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style/index.scss';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
