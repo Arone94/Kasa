c6a8fbdc9fcba38ad5af923db50c619c3c84090e
Arone Marchet <arone.marchet@gmail.com>
2022-11-21 16:03:45 +0100

    code header, scss pour nav et footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import {Routes, Route} from "react-router-dom";
-import './style/index.scss';
-import Home from './pages/Home';
-import About from './pages/About';
-import Logements from './pages/Logements';
-import Error from './pages/Error';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages//Home";
+import About from "./pages//About";
+import Header from "./components/Header";
+import FicheLogement from "./pages/FicheLogement";
+import Footer from "./components/Footer";
+import Error from "./pages/Error";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-        <Routes>
-          <Route path="/" element={ <Home /> }/>
-          <Route path='/a-propos' element={ <About /> }/>
-          <Route path='/logement/:id' element={ <Logements /> }/>
-          <Route path='*' element={ <Error /> }/>
-        </Routes>
-      <Footer/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<main>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/logement/:id" element={<FicheLogement />} />
+					<Route path="*" element={<Error />} />
+				</Routes>
+			</main>
+			<Footer />
+		</div>
+	);
 }
+
 export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import logoMobileWhite from '../images/logoMobileFooter.png'
 
-const Footer = () => {
-    return (
-        <footer>
-            <img src={logoMobileWhite} alt="Logo de Kasa" />
-            <span>© 2020 Kasa. All rights reserved</span>
-        </footer>
-    );
+import logo from "../assets/images/logo-footer.svg";
+
+export default function Footer() {
+	return (
+		<footer className="foot-container">
+			<img src={logo} alt="logo de kasa" />
+			<p className="foot-container__text">© 2020 Kasa. All rights reserved</p>
+		</footer>
+	);
 }
-
-export default Footer;
