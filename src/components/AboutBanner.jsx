60448cb5260c74efcd15e9432705ac7d6a245134
Arone Marchet <arone.marchet@gmail.com>
2022-11-22 16:32:45 +0100

    correctif chemin

--- src/components/AboutBanner.jsx
+++ src/components/AboutBanner.jsx
@@ -1,4 +1,4 @@
-import "../../style/main.scss";
+import "../style/main.scss";
 //la bannière about est une image et un claque assombrissant qui varient selon le responsive
 export default function AboutBanner() {
 	return <div className="aboutBackground wh-msk"></div>;
